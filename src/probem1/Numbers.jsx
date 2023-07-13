473de1953f1a19686eb7c297ab5c793c48fdcbad
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-07-13 11:27:09 +0530

    returns as a json object

--- src/probem1/Numbers.jsx
+++ src/probem1/Numbers.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const Numbers = () => {
-  // output that holds numbers
   const [numbers, setNumbers] = useState([]);
+
   const handleNumbers = async () => {
     const urls = [
       "http://20.244.56.144/numbers/primes",
@@ -11,28 +11,37 @@ const Numbers = () => {
       "http://20.244.56.144/numbers/odd",
       "http://20.244.56.144/numbers/rand",
     ];
-    // store responses from the urls
+
     try {
       const responses = await Promise.all(urls.map((url) => axios.get(url)));
-      console.log(responses);
-      //  pick out only successfully fetched urlss
+
       const numbersData = responses
         .filter((response) => response.status === 200)
         .map((response) => response.data);
-      console.log(numbersData);
-      setNumbers(numbersData);
+
+      const sorted = extractNumbers(numbersData);
+      setNumbers(sorted);
     } catch (error) {
       console.error("Error retrieving numbers:", error);
     }
   };
+
+  const extractNumbers = (jsonData) => {
+    const numbers = jsonData.reduce(
+      (acc, curr) => [...acc, ...curr.numbers],
+      []
+    );
+
+    const uniqueNumbers = [...new Set(numbers)];
+    const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
+    // Return numbers as a JSON object
+    return { numbers: sortedNumbers };
+  };
+
   return (
     <div className="App">
       <button onClick={handleNumbers}>Get Numbers</button>
-      <ul>
-        {numbers.map((numbersData, index) => (
-          <li key={index}>{JSON.stringify(numbersData)}</li>
-        ))}
-      </ul>
+      <pre>{JSON.stringify(numbers, null, 2)}</pre>
     </div>
   );
 };
