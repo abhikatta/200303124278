2ab4e1f085eeb0a9d6a6bc400918eab016d180d8
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-07-13 11:12:06 +0530

    hard coded urls

--- src/probem1/Numbers.jsx
+++ src/probem1/Numbers.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-function Numbers() {
+const Numbers = () => {
+  // output that holds numbers
   const [numbers, setNumbers] = useState([]);
-
   const handleNumbers = async () => {
     const urls = [
       "http://20.244.56.144/numbers/primes",
@@ -11,20 +11,20 @@ function Numbers() {
       "http://20.244.56.144/numbers/odd",
       "http://20.244.56.144/numbers/rand",
     ];
-
+    // store responses from the urls
     try {
       const responses = await Promise.all(urls.map((url) => axios.get(url)));
-
+      console.log(responses);
+      //  pick out only successfully fetched urlss
       const numbersData = responses
         .filter((response) => response.status === 200)
         .map((response) => response.data);
-
+      console.log(numbersData);
       setNumbers(numbersData);
     } catch (error) {
       console.error("Error retrieving numbers:", error);
     }
   };
-
   return (
     <div className="App">
       <button onClick={handleNumbers}>Get Numbers</button>
@@ -35,6 +35,6 @@ function Numbers() {
       </ul>
     </div>
   );
-}
+};
 
 export default Numbers;
